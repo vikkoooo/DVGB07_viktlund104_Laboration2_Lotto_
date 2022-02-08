0b1427a4162f2ff3ed1f4ca65ac3586a343fbe19
Viktor Lundberg <vikkolundberg@gmail.com>
2022-02-08 14:04:22 +0100

    first commit

---
+++ DVGB07_viktlund104_Laboration2_Lotto/MainForm.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace DVGB07_viktlund104_Laboration2_Lotto
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.yourLottoLabel = new System.Windows.Forms.Label();
+			this.firstNumberTextBox = new System.Windows.Forms.TextBox();
+			this.secondNumberTextBox = new System.Windows.Forms.TextBox();
+			this.thirdNumberTextBox = new System.Windows.Forms.TextBox();
+			this.fourthNumberTextBox = new System.Windows.Forms.TextBox();
+			this.fifthNumberTextBox = new System.Windows.Forms.TextBox();
+			this.sixthNumberTextBox = new System.Windows.Forms.TextBox();
+			this.seventhNumberTextBox = new System.Windows.Forms.TextBox();
+			this.numberLottosLabel = new System.Windows.Forms.Label();
+			this.numberLottosTextBox = new System.Windows.Forms.TextBox();
+			this.startLottoButton = new System.Windows.Forms.Button();
+			this.fiveCorrectLabel = new System.Windows.Forms.Label();
+			this.fiveCorrectTextBox = new System.Windows.Forms.TextBox();
+			this.sixCorrectLabel = new System.Windows.Forms.Label();
+			this.sixCorrectTextBox = new System.Windows.Forms.TextBox();
+			this.sevenCorrectLabel = new System.Windows.Forms.Label();
+			this.sevenCorrectTextBox = new System.Windows.Forms.TextBox();
+			this.threeCorrectLabel = new System.Windows.Forms.Label();
+			this.twoCorrectTextBox = new System.Windows.Forms.TextBox();
+			this.fourCorrectTextBox = new System.Windows.Forms.TextBox();
+			this.threeCorrectTextBox = new System.Windows.Forms.TextBox();
+			this.twoCorrectLabel = new System.Windows.Forms.Label();
+			this.fourCorrectLabel = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// yourLottoLabel
+			// 
+			this.yourLottoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+			this.yourLottoLabel.Location = new System.Drawing.Point(27, 37);
+			this.yourLottoLabel.Name = "yourLottoLabel";
+			this.yourLottoLabel.Size = new System.Drawing.Size(100, 23);
+			this.yourLottoLabel.TabIndex = 0;
+			this.yourLottoLabel.Text = "Din lottorad:";
+			// 
+			// firstNumberTextBox
+			// 
+			this.firstNumberTextBox.Location = new System.Drawing.Point(133, 36);
+			this.firstNumberTextBox.Name = "firstNumberTextBox";
+			this.firstNumberTextBox.Size = new System.Drawing.Size(57, 20);
+			this.firstNumberTextBox.TabIndex = 1;
+			this.firstNumberTextBox.Text = "4";
+			// 
+			// secondNumberTextBox
+			// 
+			this.secondNumberTextBox.Location = new System.Drawing.Point(196, 36);
+			this.secondNumberTextBox.Name = "secondNumberTextBox";
+			this.secondNumberTextBox.Size = new System.Drawing.Size(57, 20);
+			this.secondNumberTextBox.TabIndex = 2;
+			this.secondNumberTextBox.Text = "7";
+			// 
+			// thirdNumberTextBox
+			// 
+			this.thirdNumberTextBox.Location = new System.Drawing.Point(258, 36);
+			this.thirdNumberTextBox.Name = "thirdNumberTextBox";
+			this.thirdNumberTextBox.Size = new System.Drawing.Size(57, 20);
+			this.thirdNumberTextBox.TabIndex = 1;
+			this.thirdNumberTextBox.Text = "10";
+			// 
+			// fourthNumberTextBox
+			// 
+			this.fourthNumberTextBox.Location = new System.Drawing.Point(321, 36);
+			this.fourthNumberTextBox.Name = "fourthNumberTextBox";
+			this.fourthNumberTextBox.Size = new System.Drawing.Size(57, 20);
+			this.fourthNumberTextBox.TabIndex = 2;
+			this.fourthNumberTextBox.Text = "13";
+			// 
+			// fifthNumberTextBox
+			// 
+			this.fifthNumberTextBox.Location = new System.Drawing.Point(384, 36);
+			this.fifthNumberTextBox.Name = "fifthNumberTextBox";
+			this.fifthNumberTextBox.Size = new System.Drawing.Size(57, 20);
+			this.fifthNumberTextBox.TabIndex = 2;
+			this.fifthNumberTextBox.Text = "20";
+			// 
+			// sixthNumberTextBox
+			// 
+			this.sixthNumberTextBox.Location = new System.Drawing.Point(446, 36);
+			this.sixthNumberTextBox.Name = "sixthNumberTextBox";
+			this.sixthNumberTextBox.Size = new System.Drawing.Size(57, 20);
+			this.sixthNumberTextBox.TabIndex = 1;
+			this.sixthNumberTextBox.Text = "27";
+			// 
+			// seventhNumberTextBox
+			// 
+			this.seventhNumberTextBox.Location = new System.Drawing.Point(509, 36);
+			this.seventhNumberTextBox.Name = "seventhNumberTextBox";
+			this.seventhNumberTextBox.Size = new System.Drawing.Size(57, 20);
+			this.seventhNumberTextBox.TabIndex = 2;
+			this.seventhNumberTextBox.Text = "34";
+			// 
+			// numberLottosLabel
+			// 
+			this.numberLottosLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+			this.numberLottosLabel.Location = new System.Drawing.Point(133, 100);
+			this.numberLottosLabel.Name = "numberLottosLabel";
+			this.numberLottosLabel.Size = new System.Drawing.Size(132, 23);
+			this.numberLottosLabel.TabIndex = 0;
+			this.numberLottosLabel.Text = "Antal dragningar:";
+			// 
+			// numberLottosTextBox
+			// 
+			this.numberLottosTextBox.Location = new System.Drawing.Point(278, 99);
+			this.numberLottosTextBox.Name = "numberLottosTextBox";
+			this.numberLottosTextBox.Size = new System.Drawing.Size(163, 20);
+			this.numberLottosTextBox.TabIndex = 3;
+			this.numberLottosTextBox.Text = "1";
+			// 
+			// startLottoButton
+			// 
+			this.startLottoButton.Location = new System.Drawing.Point(464, 97);
+			this.startLottoButton.Name = "startLottoButton";
+			this.startLottoButton.Size = new System.Drawing.Size(75, 23);
+			this.startLottoButton.TabIndex = 4;
+			this.startLottoButton.Text = "Starta Lotto";
+			this.startLottoButton.UseVisualStyleBackColor = true;
+			this.startLottoButton.Click += new System.EventHandler(this.startLottoButton_Click);
+			// 
+			// fiveCorrectLabel
+			// 
+			this.fiveCorrectLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+			this.fiveCorrectLabel.Location = new System.Drawing.Point(139, 212);
+			this.fiveCorrectLabel.Name = "fiveCorrectLabel";
+			this.fiveCorrectLabel.Size = new System.Drawing.Size(52, 23);
+			this.fiveCorrectLabel.TabIndex = 0;
+			this.fiveCorrectLabel.Text = "5 rätt:";
+			// 
+			// fiveCorrectTextBox
+			// 
+			this.fiveCorrectTextBox.Location = new System.Drawing.Point(187, 211);
+			this.fiveCorrectTextBox.Name = "fiveCorrectTextBox";
+			this.fiveCorrectTextBox.ReadOnly = true;
+			this.fiveCorrectTextBox.Size = new System.Drawing.Size(65, 20);
+			this.fiveCorrectTextBox.TabIndex = 5;
+			// 
+			// sixCorrectLabel
+			// 
+			this.sixCorrectLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+			this.sixCorrectLabel.Location = new System.Drawing.Point(264, 212);
+			this.sixCorrectLabel.Name = "sixCorrectLabel";
+			this.sixCorrectLabel.Size = new System.Drawing.Size(52, 23);
+			this.sixCorrectLabel.TabIndex = 0;
+			this.sixCorrectLabel.Text = "6 rätt:";
+			// 
+			// sixCorrectTextBox
+			// 
+			this.sixCorrectTextBox.Location = new System.Drawing.Point(312, 210);
+			this.sixCorrectTextBox.Name = "sixCorrectTextBox";
+			this.sixCorrectTextBox.ReadOnly = true;
+			this.sixCorrectTextBox.Size = new System.Drawing.Size(65, 20);
+			this.sixCorrectTextBox.TabIndex = 5;
+			// 
+			// sevenCorrectLabel
+			// 
+			this.sevenCorrectLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+			this.sevenCorrectLabel.Location = new System.Drawing.Point(389, 212);
+			this.sevenCorrectLabel.Name = "sevenCorrectLabel";
+			this.sevenCorrectLabel.Size = new System.Drawing.Size(52, 23);
+			this.sevenCorrectLabel.TabIndex = 0;
+			this.sevenCorrectLabel.Text = "7 rätt:";
+			// 
+			// sevenCorrectTextBox
+			// 
+			this.sevenCorrectTextBox.Location = new System.Drawing.Point(438, 209);
+			this.sevenCorrectTextBox.Name = "sevenCorrectTextBox";
+			this.sevenCorrectTextBox.ReadOnly = true;
+			this.sevenCorrectTextBox.Size = new System.Drawing.Size(65, 20);
+			this.sevenCorrectTextBox.TabIndex = 5;
+			// 
+			// threeCorrectLabel
+			// 
+			this.threeCorrectLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+			this.threeCorrectLabel.Location = new System.Drawing.Point(264, 175);
+			this.threeCorrectLabel.Name = "threeCorrectLabel";
+			this.threeCorrectLabel.Size = new System.Drawing.Size(52, 23);
+			this.threeCorrectLabel.TabIndex = 0;
+			this.threeCorrectLabel.Text = "3 rätt:";
+			// 
+			// twoCorrectTextBox
+			// 
+			this.twoCorrectTextBox.Location = new System.Drawing.Point(187, 174);
+			this.twoCorrectTextBox.Name = "twoCorrectTextBox";
+			this.twoCorrectTextBox.ReadOnly = true;
+			this.twoCorrectTextBox.Size = new System.Drawing.Size(65, 20);
+			this.twoCorrectTextBox.TabIndex = 5;
+			// 
+			// fourCorrectTextBox
+			// 
+			this.fourCorrectTextBox.Location = new System.Drawing.Point(438, 172);
+			this.fourCorrectTextBox.Name = "fourCorrectTextBox";
+			this.fourCorrectTextBox.ReadOnly = true;
+			this.fourCorrectTextBox.Size = new System.Drawing.Size(65, 20);
+			this.fourCorrectTextBox.TabIndex = 5;
+			// 
+			// threeCorrectTextBox
+			// 
+			this.threeCorrectTextBox.Location = new System.Drawing.Point(312, 172);
+			this.threeCorrectTextBox.Name = "threeCorrectTextBox";
+			this.threeCorrectTextBox.ReadOnly = true;
+			this.threeCorrectTextBox.Size = new System.Drawing.Size(65, 20);
+			this.threeCorrectTextBox.TabIndex = 5;
+			// 
+			// twoCorrectLabel
+			// 
+			this.twoCorrectLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+			this.twoCorrectLabel.Location = new System.Drawing.Point(138, 177);
+			this.twoCorrectLabel.Name = "twoCorrectLabel";
+			this.twoCorrectLabel.Size = new System.Drawing.Size(52, 23);
+			this.twoCorrectLabel.TabIndex = 0;
+			this.twoCorrectLabel.Text = "2 rätt:";
+			// 
+			// fourCorrectLabel
+			// 
+			this.fourCorrectLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+			this.fourCorrectLabel.Location = new System.Drawing.Point(390, 175);
+			this.fourCorrectLabel.Name = "fourCorrectLabel";
+			this.fourCorrectLabel.Size = new System.Drawing.Size(52, 23);
+			this.fourCorrectLabel.TabIndex = 0;
+			this.fourCorrectLabel.Text = "4 rätt:";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(634, 279);
+			this.Controls.Add(this.fourCorrectTextBox);
+			this.Controls.Add(this.sevenCorrectTextBox);
+			this.Controls.Add(this.twoCorrectTextBox);
+			this.Controls.Add(this.threeCorrectTextBox);
+			this.Controls.Add(this.sixCorrectTextBox);
+			this.Controls.Add(this.fiveCorrectTextBox);
+			this.Controls.Add(this.startLottoButton);
+			this.Controls.Add(this.numberLottosTextBox);
+			this.Controls.Add(this.seventhNumberTextBox);
+			this.Controls.Add(this.fourthNumberTextBox);
+			this.Controls.Add(this.sixthNumberTextBox);
+			this.Controls.Add(this.thirdNumberTextBox);
+			this.Controls.Add(this.fifthNumberTextBox);
+			this.Controls.Add(this.secondNumberTextBox);
+			this.Controls.Add(this.sevenCorrectLabel);
+			this.Controls.Add(this.twoCorrectLabel);
+			this.Controls.Add(this.fourCorrectLabel);
+			this.Controls.Add(this.threeCorrectLabel);
+			this.Controls.Add(this.firstNumberTextBox);
+			this.Controls.Add(this.sixCorrectLabel);
+			this.Controls.Add(this.numberLottosLabel);
+			this.Controls.Add(this.fiveCorrectLabel);
+			this.Controls.Add(this.yourLottoLabel);
+			this.Name = "MainForm";
+			this.Text = "Lotto 1.0";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+
+		private System.Windows.Forms.TextBox threeCorrectTextBox;
+		private System.Windows.Forms.Label twoCorrectLabel;
+		private System.Windows.Forms.Label fourCorrectLabel;
+
+		private System.Windows.Forms.Label threeCorrectLabel;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox twoCorrectTextBox;
+		private System.Windows.Forms.TextBox fourCorrectTextBox;
+		private System.Windows.Forms.TextBox fourCorrectText;
+
+		private System.Windows.Forms.Label sixCorrectLabel;
+		private System.Windows.Forms.TextBox sixCorrectTextBox;
+		private System.Windows.Forms.Label sevenCorrectLabel;
+		private System.Windows.Forms.TextBox sevenCorrectTextBox;
+
+		private System.Windows.Forms.TextBox fiveCorrectTextBox;
+
+		private System.Windows.Forms.Label fiveCorrectLabel;
+
+		private System.Windows.Forms.Button startLottoButton;
+
+		private System.Windows.Forms.TextBox numberLottosTextBox;
+
+		private System.Windows.Forms.Label numberLottosLabel;
+
+		private System.Windows.Forms.TextBox fifthNumberTextBox;
+		private System.Windows.Forms.TextBox sixthNumberTextBox;
+		private System.Windows.Forms.TextBox seventhNumberTextBox;
+
+		private System.Windows.Forms.TextBox thirdNumberTextBox;
+		private System.Windows.Forms.TextBox fourthNumberTextBox;
+
+		private System.Windows.Forms.TextBox secondNumberTextBox;
+
+		private System.Windows.Forms.TextBox firstNumberTextBox;
+
+		private System.Windows.Forms.Label yourLottoLabel;
+
+		#endregion
+	}
+}
---
+++ DVGB07_viktlund104_Laboration2_Lotto/MainForm.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVGB07_viktlund104_Laboration2_Lotto
+{
+	public partial class MainForm : Form
+	{
+		// Settings
+		private int minBound = 1; // low bound, inclusive
+		private int maxBound = 35; // high bound, inclusive
+		private int minSimulations = 1; //inclusive
+		private int maxSimulations = 999999; //inclusive
+		private int n = 0; // how many times to simulate
+		private int lottoNumbers = 7; // how many lotto numbers to pull
+		private List<int> choices;
+		private Random rand = new Random();
+		private int twoCorrect;
+		private int threeCorrect;
+		private int fourCorrect;
+		private int fiveCorrect;
+		private int sixCorrect;
+		private int sevenCorrect;
+
+		// Initialize Form
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		// When we click the button, start the lotto
+		private void startLottoButton_Click(object sender, EventArgs e)
+		{
+			// Initialize counts
+			fiveCorrect = 0;
+			sixCorrect = 0;
+			sevenCorrect = 0;
+
+			// Check that the user has given us good inputs
+			if (!allInBounds())
+			{
+				// out of bounds, abort
+				return;
+			}
+
+			if (noDuplicates(choices))
+			{
+				// duplicates found, abort
+				return;
+			}
+
+			if (!numberOfTriesInBounds())
+			{
+				// number of tries out of bounds, abort
+				return;
+			}
+
+			// We got OK inputs. Run the test
+			generateLotto(n);
+
+			// Display results
+			twoCorrectTextBox.Text = twoCorrect.ToString();
+			threeCorrectTextBox.Text = threeCorrect.ToString();
+			fourCorrectTextBox.Text = fourCorrect.ToString();
+			fiveCorrectTextBox.Text = fiveCorrect.ToString();
+			sixCorrectTextBox.Text = sixCorrect.ToString();
+			sevenCorrectTextBox.Text = sevenCorrect.ToString();
+		}
+
+		// Creates the list of choices and tries to check the user input.
+		private bool allInBounds()
+		{
+			choices = new List<int>(7); //initialize list here so we start with a fresh list every click
+
+			if (!inBounds(firstNumberTextBox.Text))
+			{
+				// This means we didn't pass the first test, abort
+				return false;
+			}
+
+			if (!inBounds(secondNumberTextBox.Text))
+			{
+				return false;
+			}
+
+			if (!inBounds(thirdNumberTextBox.Text))
+			{
+				return false;
+			}
+
+			if (!inBounds(fourthNumberTextBox.Text))
+			{
+				return false;
+			}
+
+			if (!inBounds(fifthNumberTextBox.Text))
+			{
+				return false;
+			}
+
+			if (!inBounds(sixthNumberTextBox.Text))
+			{
+				return false;
+			}
+
+			if (!inBounds(seventhNumberTextBox.Text))
+			{
+				return false;
+			}
+
+			// We passed the test, return true
+			return true;
+		}
+
+		// Parses the user inputs to int numbers. Returns true if successful
+		private bool inBounds(string text)
+		{
+			int number = 0;
+
+			try
+			{
+				number = int.Parse(text);
+			}
+			catch (Exception e)
+			{
+				MessageBox.Show(e.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return false;
+			}
+
+			if (number < minBound)
+			{
+				MessageBox.Show($"Please enter number only larger than {minBound - 1}", "Error", MessageBoxButtons.OK,
+					MessageBoxIcon.Error);
+				return false;
+			}
+
+			if (number > maxBound)
+			{
+				MessageBox.Show($"Please enter number only smaller than {maxBound + 1}", "Error", MessageBoxButtons.OK,
+					MessageBoxIcon.Error);
+				return false;
+			}
+
+			choices.Add(number); // add number to the list
+
+			return true;
+		}
+
+		// Checks for duplicates in the list, true if no duplicates. false if duplicates found
+		private bool noDuplicates(List<int> list)
+		{
+			// Check for duplicates. Distinct will find number of unique values, needs to NOT be the same as total list
+			if (list.Count != list.Distinct().Count())
+			{
+				MessageBox.Show("Please choose only unique values, no duplicates", "Error", MessageBoxButtons.OK,
+					MessageBoxIcon.Error);
+				return true;
+			}
+
+			return false;
+		}
+
+
+		// Parses number of tries to user input. True if success, false if user provided us with some wrong inputs
+		private bool numberOfTriesInBounds()
+		{
+			try
+			{
+				n = int.Parse(numberLottosTextBox.Text);
+			}
+			catch (Exception e)
+			{
+				MessageBox.Show(e.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return false;
+			}
+
+			if (n < minSimulations)
+			{
+				MessageBox.Show($"Please enter number of tries larger than {minSimulations - 1}", "Error",
+					MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return false;
+			}
+
+			if (n > maxSimulations)
+			{
+				MessageBox.Show($"Please enter number of tries smaller than {maxSimulations + 1}", "Error",
+					MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return false;
+			}
+
+			return true;
+		}
+
+
+		// Generates a new lotto draw. It then checks how many the user had correct, before it calls itself again
+		private int generateLotto(int n)
+		{
+			if (n == 0)
+			{
+				return 0;
+			}
+
+			List<int> possibleNumbers = new List<int>(maxBound - minBound);
+			List<int> winningNumbers = new List<int>(lottoNumbers);
+
+			// fill list with possible drawings
+			for (int i = minBound; i <= maxBound; i++)
+			{
+				possibleNumbers.Add(i);
+			}
+
+			// pull out a number using the indexes of the list
+			for (int i = 0; i < lottoNumbers; i++)
+			{
+				int draw = rand.Next(possibleNumbers.Count); // get index of number to pull out
+
+				winningNumbers.Add(possibleNumbers[draw]); // add winning number to winning list
+				possibleNumbers.RemoveAt(draw); // remove already drawn number from being drawn again
+			}
+			
+			// check user score
+			int score = checkUserScore(winningNumbers);
+			// int score = checkUserScoreOptimized(winningNumbers);
+			
+			// Assign score to our counter class variables
+			if (score == 2)
+			{
+				twoCorrect++;
+			}
+			
+			if (score == 3)
+			{
+				threeCorrect++;
+			}
+
+			if (score == 4)
+			{
+				fourCorrect++;
+			}
+
+			if (score == 5)
+			{
+				fiveCorrect++;
+			}
+
+			if (score == 6)
+			{
+				sixCorrect++;
+			}
+
+			if (score == 7)
+			{
+				sevenCorrect++;
+			}
+
+			return generateLotto(n - 1);
+		}
+
+
+		// Checks how many correct numbers the user had in his choices, returns the amount of correct numbers.
+		private int checkUserScore(List<int> winningNumbers_)
+		{
+			int score = 0;
+
+			foreach (var e in winningNumbers_)
+			{
+				if (choices.Contains(e))
+				{
+					score++;
+				}
+			}
+
+			return score;
+		}
+		
+	}
+}
